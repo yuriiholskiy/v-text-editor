babd8f9eaf585898eb75766627bdeb056b6a8a59
Yurii <mors.lviv@gmail.com>
2020-04-21 23:10:22 +0300

    add some comments + some styles

--- src/App.vue
+++ src/App.vue
@@ -129,8 +129,12 @@ body {
   padding: 0;
   background-color: rgba(255, 255, 255, 0.7);
 }
+
 label {
   display: inline-block;
+}
+#app {
+  padding-bottom: 1rem;
 }
 .container {
   max-width: 1024px;
